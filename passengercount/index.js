4347bba303a687d44a0648c6b4e81133fa90a478
Kaushik <kmzdr1@gmail.com>
2022-12-24 19:00:08 +0530

    update: functionality checked

--- passengercount/index.js
+++ passengercount/index.js
@@ -1,2 +1,16 @@
-let myAge=27;
-console.log(myAge)
+let countNum=document.getElementById("count-el")
+let countSave=document.getElementById("save-el")
+let count=0
+
+function increment(){
+    count=count+1
+    countNum.innerText=count
+}
+
+function save(){
+   let countStr=count + " - "
+   countSave.textContent += countStr
+   countNum.textContent=0
+   count=0
+
+}
