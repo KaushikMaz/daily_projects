5bbedf6295d309cbd50374c087bf60d5ebeffa35
Kaushik <kmzdr1@gmail.com>
2023-01-21 12:44:02 +0530

    update:initialization of blackjack game

---
+++ blackjack/index.js
@@ -0,0 +1,20 @@
+let firstCard=10;
+let secondCard=11;
+
+let sum=firstCard + secondCard;
+let hasBlackJack= false;
+let isAlive=true;
+
+let message=" ";
+
+if( sum<=20){
+    message="Do you wnat to draw a new Card?"
+} else if( sum===21){
+    message="Wooow! You got the blackJack!!"
+    hasBlackJack= true;
+}else {
+    message="You are out of the game!!"
+    isAlive=false;
+}
+
+console.log(message)
