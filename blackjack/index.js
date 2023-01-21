affbdbecc8a90b03c910ae93c028d52725551ab1
Kaushik <kmzdr1@gmail.com>
2023-01-22 00:44:23 +0530

    update: code refactor

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,22 +1,41 @@
 let firstCard=10;
-let secondCard=11;
-
+let secondCard=09;
+let cards=[firstCard,secondCard]
 let sum=firstCard + secondCard;
 let hasBlackJack= false;
 let isAlive=true;
 
 let message=" ";
+
+let messageEl= document.getElementById("message")
+let sumEl=document.querySelector("#sum")
+let cardsEl=document.getElementById("cards")
+
 const startGame=()=>{
+    renderGame()
+}
+
+const renderGame=()=>{
+    cardsEl.textContent="Cards:" + cards[0] + " "+","+ " "+ cards[1];
+    sumEl.textContent="Sum:" + sum;
+
 
     if( sum<=20){
         message="Do you wnat to draw a new Card?"
     } else if( sum===21){
-        message="Wooow! You got the blackJack!!"
+        message=" You got the blackJack!!"
         hasBlackJack= true;
     }else {
         message="You are out of the game!!"
         isAlive=false;
     }
 
-    console.log(message)
+    messageEl.textContent=message;
+}
+
+const newCard=()=>{
+    let card=6
+    sum+= card;
+    cards.push(card)
+    renderGame();
 }
