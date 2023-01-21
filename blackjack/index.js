069feb484d418f9353e89718beb3ef129fb6d0f3
Kaushik <kmzdr1@gmail.com>
2023-01-21 14:03:21 +0530

    update: logic initialised

--- blackjack/index.js
+++ blackjack/index.js
@@ -6,15 +6,17 @@ let hasBlackJack= false;
 let isAlive=true;
 
 let message=" ";
+const startGame=()=>{
 
-if( sum<=20){
-    message="Do you wnat to draw a new Card?"
-} else if( sum===21){
-    message="Wooow! You got the blackJack!!"
-    hasBlackJack= true;
-}else {
-    message="You are out of the game!!"
-    isAlive=false;
+    if( sum<=20){
+        message="Do you wnat to draw a new Card?"
+    } else if( sum===21){
+        message="Wooow! You got the blackJack!!"
+        hasBlackJack= true;
+    }else {
+        message="You are out of the game!!"
+        isAlive=false;
+    }
+
+    console.log(message)
 }
-
-console.log(message)
