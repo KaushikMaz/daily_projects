1848ac3527bab3d5d99d765f24c51997a33acce8
Kaushik <kmzdr1@gmail.com>
2023-01-24 23:22:55 +0530

    Refactor: Refactored onclick to addeventlistener

--- chrome_extension/index.js
+++ chrome_extension/index.js
@@ -1,3 +1,5 @@
-const saveLead=()=>{
-    console.log("Button Clicked!!")
-}
+
+let inputBtn= document.getElementById("input-btn");
+inputBtn.addEventListener("click",saveLead=()=>{
+    console.log("Button clicked through Event Handler")
+})
