138e6614523222abd224ef69064731e5ddec7624
Kaushik <kmzdr1@gmail.com>
2023-01-25 16:47:34 +0530

    refactor: rendering inputText values

--- chrome_extension/index.js
+++ chrome_extension/index.js
@@ -4,11 +4,13 @@ const inputText=document.getElementById("input-el")
 const inputBtn= document.getElementById("input-btn");
 const ulEl=document.getElementById("ul");
 
-inputBtn.addEventListener("click",saveLead=()=>{
+inputBtn.addEventListener("click",()=>{
     myLeads.push(inputText.value)
-    console.log(myLeads)
+    for (let i=0; i<myLeads.length ; i++){
+        ulEl.innerHTML+="<li>" + myLeads[i] + "</li>"
+        
+    }
+    
+    
 })
 
-for (let i=0;i<myLeads.length;i++){
-    ulEl.innerHTML+="<li> myLeads[i] </li>"
-}
