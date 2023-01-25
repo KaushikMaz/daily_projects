d2e1f2fd33dd60aa3c4a24d81a97e6069d2d64ad
Kaushik <kmzdr1@gmail.com>
2023-01-25 19:32:02 +0530

    Optimization: Performance optimization to reduce DOM manipulation to only 1 time

--- chrome_extension/index.js
+++ chrome_extension/index.js
@@ -6,15 +6,15 @@ const ulEl=document.getElementById("ul");
 
 inputBtn.addEventListener("click",()=>{
     myLeads.push(inputText.value)
+    let listItems=""
     for (let i=0; i<myLeads.length ; i++){
-        ulEl.innerHTML+="<li>" + myLeads[i] + "</li>"
+        listItems+="<li>" + myLeads[i] + "</li>"
         /*const li=document.createElement("li")
         li.textContent=myLeads[i]
         ulEl.append(li)*/
-
-        
+               
     }
-    
+    ulEl.innerHTML=listItems; 
     
 })
 
