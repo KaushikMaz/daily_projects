9d7d812445b5f526c4d209022ea1b066f7adf4be
Kaushik <kmzdr1@gmail.com>
2023-04-07 23:22:32 +0530

    Update: Added functionality to get data from local storage and save it in the input array

--- chrome_extension/index.js
+++ chrome_extension/index.js
@@ -4,13 +4,20 @@ const inputText=document.getElementById("input-el")
 const inputBtn= document.getElementById("input-btn");
 const ulEl=document.getElementById("ul");
 
+let leadsFromStorage=JSON.parse(localStorage.getItem("myLeads"))
+
+if(leadsFromStorage){
+    myLeads=leadsFromStorage
+    render()
+}
+
 inputBtn.addEventListener("click",()=>{
     myLeads.push(inputText.value)
     inputText.value=" ";
     localStorage.setItem("myLeads",JSON.stringify(myLeads))/* saving the input array into local storage*/
     render();
     
-    console.log(localStorage.getItem("myLeads"))
+    // console.log(localStorage.getItem("myLeads"))
 })
 
 const render=()=>{
