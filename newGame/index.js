acdc0a79d43aa3661cbc7e4460cdcc6d86a03190
Kaushik <kmzdr1@gmail.com>
2023-04-13 18:52:03 +0530

    Refactor: Refactored the for loop into map function

--- newGame/index.js
+++ newGame/index.js
@@ -2,18 +2,15 @@
 
 
 
-const hero={Id:"hero",name:"hero",avatar:"/newGame/images/wizard.png",health:60,diceRoll:6,diceCount:3}
+const hero={Id:"hero",name:"hero",avatar:"/newGame/images/wizard.png",health:60,diceRoll:[6,5,2],diceCount:3}
 
-const monster={Id:"monster",name:"Orc",avatar:"/newGame/images/orc.png",health:10,diceRoll:4,diceCount:2}
+const monster={Id:"monster",name:"Orc",avatar:"/newGame/images/orc.png",health:10,diceRoll:[4,3],diceCount:2}
 
 
 
 const renderCharacter=(data)=>{
     const {Id,name,avatar,health,diceRoll,diceCount}=data;
-    let diceHtml="";
-    for (i=0;i<diceCount;i++){
-        diceHtml+=`<div class="dice">6</div>`
-    }
+    const diceHtml=diceRoll.map(x=>`<div class="dice">${x}</div>`).join("")
     document.getElementById(Id).innerHTML=
     `<div class="character-card">
         <h4 class="name">${name}</h4>
