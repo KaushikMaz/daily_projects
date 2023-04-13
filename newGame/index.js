5bd6e37f7152076c67d9ee93e67d24fa99128364
Kaushik <kmzdr1@gmail.com>
2023-04-13 21:52:39 +0530

    Refactor Refactor the code

--- newGame/index.js
+++ newGame/index.js
@@ -2,15 +2,30 @@
 
 
 
-const hero={Id:"hero",name:"hero",avatar:"/newGame/images/wizard.png",health:60,diceRoll:[6,5,2],diceCount:3}
+const hero={Id:"hero",name:"hero",avatar:"/newGame/images/wizard.png",health:60,diceCount:3}
 
-const monster={Id:"monster",name:"Orc",avatar:"/newGame/images/orc.png",health:10,diceRoll:[4,3],diceCount:2}
+const monster={Id:"monster",name:"Orc",avatar:"/newGame/images/orc.png",health:10,diceCount:2}
 
+
+function getDiceRollArray(diceCount) {
+    let newDiceRolls = [];
+    for (let i = 0; i < diceCount; i++) {
+        newDiceRolls.push(Math.floor(Math.random() * 6) + 1);
+    }
+    return newDiceRolls;
+}
+
+function getDiceHtml(diceCount){
+    return getDiceRollArray(diceCount).map(function(num){
+        return `<div class="dice">${num}</div>`
+
+    }).join("")
+}
 
 
 const renderCharacter=(data)=>{
-    const {Id,name,avatar,health,diceRoll,diceCount}=data;
-    const diceHtml=diceRoll.map(x=>`<div class="dice">${x}</div>`).join("")
+    const {Id,name,avatar,health,diceCount}=data;
+    const diceHtml=getDiceHtml(diceCount)
     document.getElementById(Id).innerHTML=
     `<div class="character-card">
         <h4 class="name">${name}</h4>
