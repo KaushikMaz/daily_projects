c0024c294159e6a1f16946bdb46906efb7ce4cfc
Kaushik <kmzdr1@gmail.com>
2023-04-13 18:11:23 +0530

    Update: Added multiple dices in the function

--- newGame/index.js
+++ newGame/index.js
@@ -2,20 +2,24 @@
 
 
 
-const hero={Id:"hero",name:"hero",avatar:"/newGame/images/wizard.png",health:60,diceRoll:6}
+const hero={Id:"hero",name:"hero",avatar:"/newGame/images/wizard.png",health:60,diceRoll:6,diceCount:3}
 
-const monster={Id:"monster",name:"Orc",avatar:"/newGame/images/orc.png",health:10,diceRoll:4}
+const monster={Id:"monster",name:"Orc",avatar:"/newGame/images/orc.png",health:10,diceRoll:4,diceCount:2}
 
 
 
 const renderCharacter=(data)=>{
-    const {Id,name,avatar,health,diceRoll}=data;
+    const {Id,name,avatar,health,diceRoll,diceCount}=data;
+    let diceHtml="";
+    for (i=0;i<diceCount;i++){
+        diceHtml+=`<div class="dice">6</div>`
+    }
     document.getElementById(Id).innerHTML=
     `<div class="character-card">
         <h4 class="name">${name}</h4>
         <img class="avatar" src="${avatar}"/>
         <p class="health">health: <b>${health}</b></p>
-        <div class="dice-container"><div class="dice"> ${diceRoll}</div></div>
+        <div class="dice-container">${diceHtml}</div>
     </div> `
 }
 
