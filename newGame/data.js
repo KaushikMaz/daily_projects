c1172b01db155859b52c66cc940604c46ffde779
Kaushik <kmzdr1@gmail.com>
2023-04-14 03:09:30 +0530

    Refactor: Refactored and exported data in other files

---
+++ newGame/data.js
@@ -0,0 +1,8 @@
+const characterData={
+    hero:{Id:"hero",name:"hero",avatar:"/newGame/images/wizard.png",health:60,diceCount:3},
+
+    monster:{Id:"monster",name:"Orc",avatar:"/newGame/images/orc.png",health:10,diceCount:2}
+
+
+}
+export default characterData;
--- newGame/index.js
+++ newGame/index.js
@@ -1,20 +1,7 @@
-
-
-
+import characterData from "../newGame/data.js"
 
 // const hero={Id:"hero",name:"hero",avatar:"/newGame/images/wizard.png",health:60,diceCount:3}
-
 // const monster={Id:"monster",name:"Orc",avatar:"/newGame/images/orc.png",health:10,diceCount:2}
-
-
-const characterData={
-    hero:{Id:"hero",name:"hero",avatar:"/newGame/images/wizard.png",health:60,diceCount:3},
-
-    monster:{Id:"monster",name:"Orc",avatar:"/newGame/images/orc.png",health:10,diceCount:2}
-
-
-}
-
 
 function getDiceRollArray(diceCount) {
     // let newDiceRolls = [];
