b968ae53be3d543b64957c2ca9c436cc82675e7d
Kaushik <kmzdr1@gmail.com>
2023-04-15 00:27:50 +0530

    Refactor: Refactored code to remove 'this', since I have already deconstructed into this

--- newGame/character.js
+++ newGame/character.js
@@ -27,15 +27,15 @@ function Character(data){
     Object.assign(this,data)
     this.diceArray=getDicePlaceholderHtml(this.diceCount)
     this.getCharacterHtml= function(){
-        const {Id,name,avatar,health,diceCount}=this;
+        const {Id,name,avatar,health,diceCount,diceArray}=this;
         const diceHtml = this.getDiceHtml(diceCount)
 
         return `<div class="character-card">
-                    <h4 class="name"> ${this.name} </h4>
-                    <img class="avatar" src="${this.avatar}" />
-                    <div class="health">health: <b> ${this.health} </b></div>
+                    <h4 class="name"> ${name} </h4>
+                    <img class="avatar" src="${avatar}" />
+                    <div class="health">health: <b> ${health} </b></div>
                     <div class="dice-container">    
-                        ${this.diceArray}
+                        ${diceArray}
                     </div>
                 </div>`;
         }
