5b25cb2d2ae04dabd8e7ef78d9c60f0bb7a9bc63
Kaushik <kmzdr1@gmail.com>
2023-04-15 00:03:05 +0530

    Refactor: Refactor the code to create a function such that the dice roll stays empty before anything

--- newGame/index.js
+++ newGame/index.js
@@ -24,7 +24,9 @@ import Character from "../newGame/character.js"
     render()
 
     
-    
+    document.getElementById("attack-button").addEventListener("click",function(){
+        render()
+    })
     
 
 
