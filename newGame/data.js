1d5637f3a9d225e494b977de77acab0b063c3df3
Kaushik <kmzdr1@gmail.com>
2023-04-16 21:09:43 +0530

    Update: Updated the data with currentDiceScore which is an empty array

--- newGame/data.js
+++ newGame/data.js
@@ -1,7 +1,7 @@
 const characterData={
-    hero:{name:"hero",avatar:"/newGame/images/wizard.png",health:60,diceCount:3},
+    hero:{name:"hero",avatar:"/newGame/images/wizard.png",health:60,diceCount:3, currentDiceScore:[]},
 
-    monster:{name:"Orc",avatar:"/newGame/images/orc.png",health:10,diceCount:2}
+    monster:{name:"Orc",avatar:"/newGame/images/orc.png",health:10,diceCount:2, currentDiceScore:[]}
 
 
 }
