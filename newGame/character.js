29c1165df5c2ec20a98fcfd052d2c765327fb4d6
Kaushik <kmzdr1@gmail.com>
2023-04-17 22:52:03 +0530

    Update: displaying the health bar

--- newGame/character.js
+++ newGame/character.js
@@ -29,11 +29,13 @@ function Character(data){
     this.getCharacterHtml= function(){
         const {Id,name,avatar,health,diceCount,diceArray}=this;
         const diceHtml = this.getDiceHtml(diceCount)
-
+        const healthBar=this.getHealthBarHtml()
+        
         return `<div class="character-card">
                     <h4 class="name"> ${name} </h4>
                     <img class="avatar" src="${avatar}" />
                     <div class="health">health: <b> ${health} </b></div>
+                    ${healthBar}
                     <div class="dice-container">    
                         ${diceArray}
                     </div>
@@ -47,7 +49,15 @@ function Character(data){
             this.health=0
             this.dead=true
         }
-        
+    this.maxHealth=this.health
+    this.getHealthBarHtml=()=>{
+        const percent=getPercentage(this.health,this.maxHealth)
+        return `<div class="health-bar-outer">
+                    <div class="health-bar-inner ${percent<26?danger:""} " 
+                    style="width: ${percent}%;">
+                    </div>
+                </div>`
+    }  
     }
     this.getDiceHtml= function(diceCount){
         this.currentDiceScore=getDiceRollArray(this.diceCount)
@@ -55,10 +65,14 @@ function Character(data){
         //     return `<div class="dice">${num}</div>`
     
         // }).join("")
-        this.diceArray=this.currentDiceScore.map((num)=>
+        this.diceArray=this.currentDiceScore.map(num=>
             `<div class="dice">${num}</div>` ).join('')
     
     }
+}
+
+const getPercentage=(remainingHealth, maximumHealth)=>{
+    (100*remainingHealth)/maximumHealth
 }
 
 export default Character;
