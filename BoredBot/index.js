73f7a30fa7a8d6281b1471a5e058aa5255edf85e
Kaushik <kmzdr1@gmail.com>
2023-04-20 18:12:38 +0530

    Update: Trying toggle and add interchangeably

--- BoredBot/index.js
+++ BoredBot/index.js
@@ -4,7 +4,7 @@ document.getElementById('btn-activity').addEventListener("click",function(){
     fetch("http://www.boredapi.com/api/activity/").then(response=>response.json())
     .then(data=>{
         document.getElementById("activity").textContent=data.activity
-        document.body.classList.add("fun")
+        document.body.classList.toggle("fun")//Trying add and toggle interchangeably
         setTimeout(()=>{
             document.body.classList.remove("fun")
         },2000)
