73161b380d9fd78a9237989f6b3e9cf91ec7c9e3
Kaushik <kmzdr1@gmail.com>
2023-04-20 22:39:33 +0530

    update: Added style to the page

--- Blogspace/index.js
+++ Blogspace/index.js
@@ -3,7 +3,7 @@ fetch("https://jsonplaceholder.typicode.com/posts").then(res=>res.json())
     const postArray= data.slice(0,5)
     console.log(postArray)
     const blogList=postArray.map(num=>{
-       return(`<h3>${num.title}</h3>
+       return(`<h2>${num.title}</h2>
                 <p>${num.body}</p>
                 <hr/>`)
     }).join(" ")
