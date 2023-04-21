45a2dc0f4cc9848f9e3f6f08307afc1567b9b015
Kaushik <kmzdr1@gmail.com>
2023-04-21 17:20:37 +0530

    Fix: update into https protocol

--- BoredBot/index.js
+++ BoredBot/index.js
@@ -1,7 +1,7 @@
 
 
 document.getElementById('btn-activity').addEventListener("click",function(){
-    fetch("http://www.boredapi.com/api/activity/")
+    fetch("https://www.boredapi.com/api/activity/")
     .then(response=>response.json())
     .then(data=>{
         document.getElementById("activity").textContent=data.activity
