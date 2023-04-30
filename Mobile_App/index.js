4ac7f5aacf207caa29d1ba1481968bb80db1eb16
Kaushik <kmzdr1@gmail.com>
2023-05-01 01:55:57 +0530

    Update: Executing onvalue function

--- Mobile_App/index.js
+++ Mobile_App/index.js
@@ -1,21 +1,43 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref , push, onValue, remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
+const appSettings={
+    databaseURL:"https://playground-b356b-default-rtdb.asia-southeast1.firebasedatabase.app"
+}
 
+const app=initializeApp(appSettings)
+const database=getDatabase(app)
+const shoppingListDb=ref(database,"shoppingList")
 
 const inputEl=document.getElementById("input-field")
 const buttonEl=document.getElementById("btn")
-const shoppingList=document.getElementById("shopping-list")
+const shoppingListEl=document.getElementById("shopping-list")
 
 const clearInput=()=>inputEl.value="";
+const clearShoppingList=()=>shoppingListEl.innerHTML="";
 const addShoppingList=(items)=>{
-    shoppingList.innerHTML+=`<li>${items}</li>`
+    shoppingListEl.innerHTML+=`<li>${items}</li>`
 }
 
 buttonEl.addEventListener("click", function(){
     let inputvalue=inputEl.value;
-    addShoppingList(inputvalue);
+    push(shoppingListDb,inputvalue)
+    // addShoppingList(inputvalue);
     clearInput();
 
 })
 
+//This function onValue executes whenever there is any change in the db
+
+onValue(shoppingListDb,function(snapshot){
+    let itemsArray=Object.values(snapshot.val()) // Converting the objects into array
+
+    clearShoppingList();// To clear the shoppinglist area after the onvalue function executes
+
+    for(let i=0;i<itemsArray.length;i++){
+        addShoppingList(itemsArray[i])
+    }
+
+
+
+})
