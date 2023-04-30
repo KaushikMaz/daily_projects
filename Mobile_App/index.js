d90c197ae0dbdd161c8972a0443ea1f523c8dcdf
Kaushik <kmzdr1@gmail.com>
2023-04-30 23:26:46 +0530

    Refactoring: refactoring of code

--- Mobile_App/index.js
+++ Mobile_App/index.js
@@ -7,9 +7,15 @@ const inputEl=document.getElementById("input-field")
 const buttonEl=document.getElementById("btn")
 const shoppingList=document.getElementById("shopping-list")
 
+const clearInput=()=>inputEl.value="";
+const addShoppingList=(items)=>{
+    shoppingList.innerHTML+=`<li>${items}</li>`
+}
+
 buttonEl.addEventListener("click", function(){
     let inputvalue=inputEl.value;
-    shoppingList.innerHTML+=`<li>${inputvalue}</li>`
-    inputEl.value=" "
+    addShoppingList(inputvalue);
+    clearInput();
 
 })
+
