07e47e626ea5ed760125092cce917dac549fc6a6
Kaushik <kmzdr1@gmail.com>
2023-05-05 00:22:40 +0530

    Fix: Fixed the missing div id

--- cardGame/index.js
+++ cardGame/index.js
@@ -4,16 +4,16 @@ const handleClick=()=>{
     .then(res=>res.json())
     .then(data=>{
         deckId=data.deck_id
-    })
+     })
 }
 document.getElementById("new-deck").addEventListener("click",handleClick)
 document.getElementById("draw-cards").addEventListener("click",()=>{
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
     .then(res=>res.json())
     .then(data=>{
-        document.getElementById.children[0].innerHTML=
+        document.getElementById("cards").children[0].innerHTML=
         `<img src=${data.cards[0].image} class="cards"/>`
-        document.getElementById.children[1].innerHTML=
+        document.getElementById("cards").children[1].innerHTML=
         `<img src=${data.cards[1].image} class="cards"/>`
     })
 })
