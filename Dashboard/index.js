aa7e9c77dda9b3016e4a1b5ed07e675f962812e0
Kaushik <kmzdr1@gmail.com>
2023-05-08 20:54:33 +0530

    Update: Added styling to API layouts

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -2,4 +2,12 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     .then(res => res.json())
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.regular})`
+        document.getElementById("author").textContent=`By:${data.user.name}`
+})
+
+fetch("https://www.coingecko.com/api/documentations/v3#/coins/dogecoin").then(
+    res=>res.json())
+    .then(data=>{
+        console.log(data)
     })
+    .catch(err=>console.error(err))
