4dc5d50b99ed47f310d1bfc5dd8c93ca7ac3cb4b
Kaushik <kmzdr1@gmail.com>
2023-05-09 00:44:36 +0530

    Update: Trying to display coin image

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -5,9 +5,28 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.getElementById("author").textContent=`By:${data.user.name}`
 })
 
+// const corsAnywhereUrl = 'https://cors-anywhere.herokuapp.com/';
+// const apiUrl = 'https://www.coingecko.com/api/documentations/v3#/coins/dogecoin';
+
+// fetch(corsAnywhereUrl + apiUrl)
+//   .then(response => response.json())
+//   .then(data => {
+//     // Handle the response data
+//     document.getElementById("crypto").innerHTML=
+//         `<img src=${data.image.small}/>
+//         <span>${data.name}</span>`
+    
+//   })
+//   .catch(err => {
+//     // Handle any errors that occur during the request
+//     console.error(err);
+//   });
+
+// This is not working due to cors policy.
 fetch("https://www.coingecko.com/api/documentations/v3#/coins/dogecoin").then(
     res=>res.json())
     .then(data=>{
-        console.log(data)
+        document.getElementById("crypto").innerHTML=
+        `<img src=${data.image.small}/>
+        <span>${data.name}</span>`
     })
-    .catch(err=>console.error(err))
