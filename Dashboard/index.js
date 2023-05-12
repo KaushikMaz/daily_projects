37cb56783899c25a060d8427f75c58b8b07049e8
Kaushik <kmzdr1@gmail.com>
2023-05-12 12:12:43 +0530

    Update: Changes to refactor the dogecoin  value display

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -9,7 +9,13 @@ fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
     .then(res=>res.json())
     .then(data=>{
         console.log(data)
-        document.getElementById("crypto").innerHTML=
+        document.getElementById("crypto-names").innerHTML=
         `<img src=${data.image.small}/>
         <span>${data.name}</span>`
+
+        document.getElementById("crypto").innerHTML += `
+            <p>🎯: $${data.market_data.current_price.usd}</p>
+            <p>👆: $${data.market_data.high_24h.usd}</p>
+            <p>👇: $${data.market_data.low_24h.usd}</p>
+        `
     })
