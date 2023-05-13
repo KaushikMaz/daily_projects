05a0c62dde3853ea460d8c5d2ac859586cb3806f
Kaushik <kmzdr1@gmail.com>
2023-05-13 23:59:45 +0530

    update: Weather icon displayed

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -26,3 +26,19 @@ document.getElementById("time").textContent=date.toLocaleTimeString("en-US",{tim
 }
 
 setInterval(getCurrentTime,1000)
+
+navigator.geolocation.getCurrentPosition(position=>{
+    // console.log(position)
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}& units=imperial`)
+    .then(res=>{
+        if(!res.ok){
+            throw Error("Weather data not available")
+        } else{
+            return res.json()
+        }
+    }).then(data=>{
+        const iconURL=`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+        document.getElementById("weather").innerHTML=`<img src=${iconURL}/>`
+        // console.log(data)
+    }).catch(err=>console.error(err))
+})
