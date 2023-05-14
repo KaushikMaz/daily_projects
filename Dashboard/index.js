21b405866e2a8f1fc595966a6efbfe84f2b93d9b
Kaushik <kmzdr1@gmail.com>
2023-05-14 12:54:57 +0530

    Update: Alignment of display of weather data

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -29,7 +29,7 @@ setInterval(getCurrentTime,1000)
 
 navigator.geolocation.getCurrentPosition(position=>{
     // console.log(position)
-    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}& units=imperial`)
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
     .then(res=>{
         if(!res.ok){
             throw Error("Weather data not available")
@@ -37,8 +37,11 @@ navigator.geolocation.getCurrentPosition(position=>{
             return res.json()
         }
     }).then(data=>{
-        const iconURL=`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
-        document.getElementById("weather").innerHTML=`<img src=${iconURL}/>`
-        // console.log(data)
+        
+        document.getElementById("weather").innerHTML=
+        `<p>${data.weather[0].description}</p>
+        <p id="weather-temp">${Math.round(data.main.temp)}°C</p>
+        <p id="weather-city">${data.name}</p>`
+        console.log(data)
     }).catch(err=>console.error(err))
 })
